a774c9893f2a32166cdf5eaa3a606bdf98443ae3
Danny Piper <djpiper28@gmail.com>
2026-02-26 16:52:57 +0000

    feat: added tests for privacy policy

---
+++ handlers/privacy_policy/privacy_test.go
@@ -0,0 +1,14 @@
+package privacypolicy_test 
+
+import (
+	"testing"
+
+	privacypolicy "github.com/Nag-s-Head/chess-league/handlers/privacy_policy"
+	"github.com/stretchr/testify/require"
+)
+
+func TestPrivacy(t *testing.T) {
+	tpl, err := privacypolicy.Render()
+	require.NoError(t, err)
+	require.NotNil(t, tpl)
+}
