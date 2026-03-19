726d3c13ee042f9cddbcd1741e1f7cb5bc2cf40b
Danny Piper <djpiper28@gmail.com>
2026-03-19 00:31:31 +0000

    feat: added lock for test utils get db to prevent race condition in migrations

--- db/test_utils/utils.go
+++ db/test_utils/utils.go
@@ -1,15 +1,20 @@
 package testutils
 
 import (
+	"sync"
 	"testing"
 
 	"github.com/Nag-s-Head/chess-league/db"
 	"github.com/stretchr/testify/require"
 )
 
+var migrationLock sync.Mutex
+
 func GetDb(t *testing.T) *db.Db {
 	t.Helper()
 
+	migrationLock.Lock()
+	defer migrationLock.Unlock()
 	db, err := db.New()
 	require.NoError(t, err)
 	return db
