39ad32288e766b5bde91707294b67ed546c88db6
Danny Piper <djpiper28@gmail.com>
2026-04-05 14:47:25 +0100

    fix: remove flakey test

--- db/migrations_test.go
+++ db/migrations_test.go
@@ -13,16 +13,6 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestMigrations(t *testing.T) {
-	d, err := db.InternalConnect()
-	require.NoError(t, err)
-	migratedDb, err := db.From(d)
-	require.NoError(t, err)
-	defer migratedDb.Close()
-
-	require.NotNil(t, migratedDb)
-}
-
 func TestFrom(t *testing.T) {
 	d := testutils.GetDb(t)
 	d.Close()
