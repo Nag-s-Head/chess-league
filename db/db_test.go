cbd6b5681c8b8e6b41d591cf968792d3500646b4
Danny Piper <djpiper28@gmail.com>
2026-04-09 13:46:52 +0100

    fix: removed race condition in tests

--- db/db_test.go
+++ db/db_test.go
@@ -3,15 +3,14 @@ package db_test
 import (
 	"testing"
 
-	"github.com/Nag-s-Head/chess-league/db"
+	testutils "github.com/Nag-s-Head/chess-league/db/test_utils"
 	"github.com/stretchr/testify/require"
 )
 
 func TestConnect(t *testing.T) {
-	database, err := db.InternalConnect()
-	require.NoError(t, err)
+	database := testutils.GetDb(t)
 	defer database.Close()
 
 	require.NotNil(t, database)
-	require.NoError(t, database.Ping())
+	require.NoError(t, database.GetSqlxDb().Ping())
 }
