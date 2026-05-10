40db0049961b9b1cb2895af6b606a717acb56e23
Danny Piper <djpiper28@gmail.com>
2026-05-10 17:17:21 +0100

    feat: added retry mechanism

--- db/test_utils/utils.go
+++ db/test_utils/utils.go
@@ -1,21 +1,32 @@
 package testutils
 
 import (
+	"errors"
 	"sync"
 	"testing"
 
 	"github.com/Nag-s-Head/chess-league/db"
-	"github.com/stretchr/testify/require"
 )
 
 var migrationLock sync.Mutex
 
-func GetDb(t *testing.T) *db.Db {
+func getDb(t *testing.T, tries int) *db.Db {
 	t.Helper()
 
 	migrationLock.Lock()
 	defer migrationLock.Unlock()
 	db, err := db.New()
-	require.NoError(t, err)
+
+	if errors.Is(err, errors.New("Cannot create migrations table")) {
+		t.Log("Migration table createion error - likely due to a race condition. Retrying...")
+		return getDb(t, tries+1)
+	}
+	return db
+}
+
+func GetDb(t *testing.T) *db.Db {
+	t.Helper()
+
+	db := getDb(t, 0)
 	return db
 }
