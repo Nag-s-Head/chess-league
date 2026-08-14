827f9cce978f4a0ee1cf32a2fe8825a25b11ff28
Danny Piper <djpiper28@gmail.com>
2026-08-14 22:33:35 +0100

    fix: 2 months

--- db/model/players.go
+++ db/model/players.go
@@ -193,7 +193,7 @@ func GetPlayersByElo(db db.Db, showDeleted bool) ([]Player, error) {
 	WHERE 
 	  (deleted=FALSE OR deleted=$1) 
 		  AND
-		liglicko2_at >= EXTRACT(EPOCH FROM (CURRENT_DATE - interval '31 days')) / (24 * 60 * 60)
+		liglicko2_at >= EXTRACT(EPOCH FROM (CURRENT_DATE - interval '62 days')) / (24 * 60 * 60)
 	ORDER BY liglicko2_rating DESC, name;`, showDeleted)
 	if err != nil {
 		return nil, errors.Join(errors.New("Cannot get players"), err)
